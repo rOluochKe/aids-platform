3bb196c551c286304619f96007eac635120f0ee4
Raymond Oluoch <rasholuoch@gmail.com>
2021-02-23 17:18:47 +0300

    more show update delete actions and model validation

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -13,6 +13,31 @@ class VolunteersController < ApplicationController
     end
   end
 
+  def update
+    @volunteer = current_user.volunteers.find(params[:id])
+    if @volunteer.update(volunteer_params)
+      render json: @volunteer
+    else
+      render json: @volunteer.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @volunteer = Volunteer.find(params[:id])
+    render json: @volunteer.as_json(except: :user_id,
+                                    include: { user: { only: %i[first_name last_name
+                                                                image] } }).merge(currentUserCanEdit: @volunteer.user.email == request.headers['uid'])
+  end
+
+  def destroy
+    @volunteer = Volunteer.find(params[:id])
+    if @volunteer.destroy
+      head :no_content, status: :ok
+    else
+      render json: @volunteer.errors, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def volunteer_params
--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,3 +1,13 @@
 class Volunteer < ApplicationRecord
+  validates_presence_of :request_type, :description, :location, :longitude, :latitude, :status, :request_due_date
+  validates :description, length: { minimum: 10, maximum: 300 }
+  validate :start_datetime_cannot_be_in_the_past
+
   belongs_to :user
+
+  private
+
+  def start_datetime_cannot_be_in_the_past
+    errors.add(:request_due_date, "can't be in the past") if request_due_date.present? && request_due_date < Time.now
+  end
 end
