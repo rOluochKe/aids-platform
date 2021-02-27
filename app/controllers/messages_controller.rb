e20373c9e5a177db843122163fc36bcf207585fb
Raymond Oluoch <rasholuoch@gmail.com>
2021-02-27 20:58:30 +0300

    adding conversation and messages feature

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,44 @@
+class MessagesController < ApplicationController
+  before_action :find_conversation
+
+  def index
+    @messages = @conversation.messages
+
+    # if @messages.length > 10
+    #   @over_ten = true
+    #   @messages = @messages[-10..]
+    # end
+
+    # if params[:m]
+    #   @over_ten = false
+    #   @messages = @conversation.messages
+    # end
+
+    # @message = @conversation.messages.new
+    render json: @messages
+  end
+
+  def create
+    @message = @conversation.messages.new(message_params)
+    # redirect_to conversation_messages_path(@conversation) if @message.save
+    if @message.save
+      render json: @message
+    else
+      render json: @message.errors, status: :unprocessable_entity
+    end
+  end
+
+  # def new
+  #   @message = @conversation.messages.new
+  # end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
+
+  def find_conversation
+    @conversation = Conversation.find(params[:conversation_id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_23_134239) do
+ActiveRecord::Schema.define(version: 2021_02_27_173915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "conversations", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "body"
+    t.bigint "conversation_id"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["conversation_id"], name: "index_messages_on_conversation_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider", default: "email", null: false
